2b8e649f96484b0d31623e24ae3b410a0710ae01
Luke McMillan <mcmillan.luke@yahoo.com.au>
2021-10-30 22:49:23 +1100

    Added  3 manager tests. all tests passed.

---
+++ __tests__/Manager.test.js
@@ -0,0 +1,19 @@
+const Manager = require("../lib/Manager");
+
+test("Set office number with constructor argument", () => {
+    const testValue = 999;
+    const e = new Manager("Chucky", 1, "chucky@test.com", testValue);
+    expect(e.officeNumber).toBe(testValue);
+});
+
+test("getRole() should return \'Manager\'", () => {
+    const testValue = "Manager";
+    const e = new Manager("Chucky", 1, "chucky@test.com", 999);
+    expect(e.getRole()).toBe(testValue);
+});
+
+test("Get office number with getOfficeNumber()", () => {
+    const testValue = 999;
+    const e = new Manager("Chucky", 1, "chucky@test.com", testValue);
+    expect(e.getOfficeNumber()).toBe(testValue);
+});
