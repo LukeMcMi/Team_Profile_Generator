f489bb682973ee99916a775847afe9aba8d3fd0f
Luke McMillan <mcmillan.luke@yahoo.com.au>
2021-10-30 13:42:19 +1100

    added vdefined variables in index.js.

---
+++ index.js
@@ -0,0 +1,7 @@
+const inquirer = require("inquirer");
+const fs = requre("fs");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const Manager = require("./lib/Manager");
+
+const employees = [];
