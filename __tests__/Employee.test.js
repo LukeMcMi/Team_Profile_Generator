a5a19bad650e311190d901d44c8c18153b6e9b46
Luke McMillan <mcmillan.luke@yahoo.com.au>
2021-10-30 21:47:32 +1100

    Test for employee class, 8 tests added. Ran tests, all pass for employee. Fail for other classes as not created yet.

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,49 @@
+// const { test } = require("@jest/core");
+const Employee = require("../lib/Employee");
+
+test("Can initiate Employee", () => {
+    const e = new Employee();
+    expect(typeof(e)).toBe("object");
+});
+
+test("Set name with costructor argument", () => {
+    const name = "Chucky"
+    const e = new Employee(name);
+    expect(e.name).toBe(name);
+});
+
+test("Set id with constructor argument", () => {
+    const testValue = 999;
+    const e = new Employee("Chucky", testValue);
+    expect(e.id).toBe(testValue);
+});
+
+test("Set email with constructor argument", () => {
+    const testValue = "Chucky@test.com";
+    const e = new Employee("chucky", 1, testValue);
+    expect(e.email).toBe(testValue);
+});
+
+test("Get name with getName()", () => {
+    const testValue = "Chucky";
+    const e = new Employee(testValue);
+    expect(e.getName()).toBe(testValue);
+});
+
+test("Get ID with getId()", () => {
+    const testValue = 999;
+    const e = new Employee("Chucky", testValue);
+    expect(e.getId()).toBe(testValue);
+});
+
+test("Get email with getEmail()", () => {
+    const testValue = "chucky@test.com";
+    const e = new Employee("Chucky", 1, testValue);
+    expect(e.getEmail()).toBe(testValue);
+});
+
+test("getRole() should return \'Employee\'", () => {
+    const testValue = "Employee";
+    const e = new Employee("Chucky", 1, "chucky@test.com");
+    expect(e.getRole()).toBe(testValue);
+});
