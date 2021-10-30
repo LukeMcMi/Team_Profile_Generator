43a3c0f72e8745cd4c8186678c8fc2db4a0123cc
Luke McMillan <mcmillan.luke@yahoo.com.au>
2021-10-30 22:02:18 +1100

    Created test for engineer, 3 added. Tested, all passed.

---
+++ __tests__/Engineer.test.js
@@ -0,0 +1,19 @@
+const Engineer = require("../lib/Engineer");
+
+test("Set GitHub account with constructor", () => {
+    const testValue = "github";
+    const e = new Engineer("Chucky", 1, "chucky@test.com", testValue);
+    expect(e.github).toBe(testValue);
+});
+
+test("getRole() should return \'Engineer\'", () => {
+    const testValue = "Engineer";
+    const e = new Engineer("Chucky", 1, "chucky@test.com", "github");
+    expect(e.getRole()).toBe(testValue);
+});
+
+test("Get GitHub username with getGithub()", () => {
+    const testValue = "github";
+    const e = new Engineer("Chucky", 1, "chucky@test.com", testValue);
+    expect(e.getGithub()).toBe(testValue);
+});
